0398339a41efa0ff4e644e688daf6af1aeb8aa45
ala-boutabia <alahaya36@gmail.com>
2023-12-24 12:01:17 +0100

    implement of useSelector hook

--- counter-with-redux/src/components/Counter.js
+++ counter-with-redux/src/components/Counter.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
   const [count, setcount] = useState(0);
-  const handleIncrement = (counter) => {
+  // useSelector hook
+  const countState = useSelector((state) => state.count);
+  console.log("The initial state:", countState);
+
+  const handleIncrement = () => {
     setcount(count + 1);
   };
-  const handleDecrement = (counter) => {
+  const handleDecrement = () => {
     setcount(count - 1);
   };
 
   return (
     <div>
-      <p>{count}</p>
+      <p>{countState}</p>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
     </div>

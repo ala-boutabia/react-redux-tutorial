1d8b8b0e3813ad07f36d80565a81679767a6997f
ala-boutabia <alahaya36@gmail.com>
2023-12-26 10:54:09 +0100

    add increment and decrement to the app

--- 01_lesson/src/features/counter/Counter.js
+++ 01_lesson/src/features/counter/Counter.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
 
 const Counter = () => {
-  return <div>Counter</div>;
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment)}>Increment +</button>
+        <button onClick={() => dispatch(decrement)}>Decrement -</button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
